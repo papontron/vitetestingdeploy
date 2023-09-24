954fbb9f3e36baa5ad7827d8cb0772f72edde1ad
unknown <jaracristhiam@gmail.com>
2023-09-23 21:32:52 -0500

    app

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./app";
 
-const root = createRoot(document.querySelector("#root") as HTMLElement);
-root.render(<App />);
+createRoot(document.querySelector("#root") as HTMLElement)
+.render(<App />);
